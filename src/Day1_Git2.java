df6fe97e7cbf9f80856475e3425a61fa88fc6c41
bekirtulek <brt161822@gmail.com>
2022-08-15 22:06:17 +0300

    20220815/1

---
+++ src/Day1_Git2.java
@@ -0,0 +1,6 @@
+public class Day1_Git2 {
+    public static void main(String[] args) {
+        System.out.println("git 2. class");
+
+    }
+}
---
+++ src/Day2_GitGitHub.java
@@ -0,0 +1,20 @@
+public class Day2_GitGitHub {
+    /*
+    1-) git init [local repo olusturmak icin yani .git ile
+    klasorumuzun icindeki dosyalari oliskilendirmek icin kullanilir]
+    2-) git add .[ working space'den yani yerelden dosyalarii stagingarea'ya
+    yani commit lemek icin beklenen yere gonderir]
+    3-) git status working space'deki ve staging area'daki degisiklikleri gosterir
+    4-)git commit-m"mesaj" [staging area'dan commit store'a dosyalarmi
+    gnermek icin kullanilir.(commit yani version yani yeni surum olusturulmus olur)]
+    5-) git push [uzak repo'ya (yani remote- github'a) gondermek icin
+    kullandigimiz kod yalniz git push komutunu direkt kullanmak istersek 1 kez
+            git remote add orgin [github adresi]
+            git push -u origin master]
+   NOT: Yukarıdaki iki komutu tek seferde kullandıktan sonra
+   ikinci commit'lerim için sadece git push kullanırız...
+
+     */
+
+}
+
