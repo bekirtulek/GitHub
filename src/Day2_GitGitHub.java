5050fb4ee067e92802b6adb5d8d9693357604bc8
bekirtulek <brt161822@gmail.com>
2022-08-15 23:15:24 +0300

    20220815/4

--- src/Day2_GitGitHub.java
+++ src/Day2_GitGitHub.java
@@ -23,6 +23,7 @@ public class Day2_GitGitHub {
     public static void main(String[] args) {
         System.out.println("branch deneme 1");
         System.out.println("master deneme 1");
+        System.out.println("branch deneme 2");
 
     }
 }
