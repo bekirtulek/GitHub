3a07f811fe18969406733d6c802ee6456acc1920
bekirtulek <brt161822@gmail.com>
2022-08-16 13:09:52 +0300

    tekrar2

--- src/Day2_Tekrar.java
+++ src/Day2_Tekrar.java
@@ -2,7 +2,7 @@ public class Day2_Tekrar {
     public static void main(String[] args) {
         System.out.println("2. gunun tekrarini yapiyorum");
         System.out.println("ben git&github'i anladim");
-        System.out.println("pull da oldumu on numara ")
+        System.out.println("pull da oldumu on numara ");
 
 
 
