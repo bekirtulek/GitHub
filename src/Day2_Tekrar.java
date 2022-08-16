1458baf807719b0702bc2b3995bfc51a57b06153
bekirtulek <brt161822@gmail.com>
2022-08-16 12:56:21 +0300

    tekrar

--- src/Day2_Tekrar.java
+++ src/Day2_Tekrar.java
@@ -1,6 +1,8 @@
 public class Day2_Tekrar {
     public static void main(String[] args) {
         System.out.println("2. gunun tekrarini yapiyorum");
+        System.out.println("ben git&github'i anladim");
+
 
 
     }
