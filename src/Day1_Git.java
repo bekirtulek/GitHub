c93ab1e0a42d90d88867ce29e884e0150ee1fdc9
bekir <110713701+bekirtulek@users.noreply.github.com>
2022-08-17 11:56:51 +0300

    Update Day1_Git.java

--- src/Day1_Git.java
+++ src/Day1_Git.java
@@ -6,6 +6,7 @@ public class Day1_Git {
         System.out.println("hello git 4. deneme");
         System.out.println("hello git 5. deneme");
         System.out.println("hello git 6. deneme");
+        System.out.println("hello git 7. deneme");
         // working space
         // staged area
         // commit store
