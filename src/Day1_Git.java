93e5441f9755b8c99c14ae6c00ba54a6df8f2f02
bekirtulek <brt161822@gmail.com>
2022-08-17 11:51:24 +0300

    20220817/

--- src/Day1_Git.java
+++ src/Day1_Git.java
@@ -2,6 +2,7 @@ public class Day1_Git {
     public static void main(String[] args) {
         System.out.println("hello git 1. deneme");
         System.out.println("hello git 2. deneme");
+        System.out.println("hello git 3. deneme");
         // working space
         // staged area
         // commit store
